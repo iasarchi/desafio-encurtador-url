c6ec5e8cb98d71108a5a0bf262e3bc197b66200d
Iasmim Cardoso <iasmimcardoso11@gmail.com>
2024-01-27 13:34:42 -0300

    feat: adicionando o parametro de timeTaken no dto de response do endpoint de encurtar URL.

--- src/main/java/com/iasarchi/encurtadorurl/controller/ShortenerController.java
+++ src/main/java/com/iasarchi/encurtadorurl/controller/ShortenerController.java
@@ -4,21 +4,28 @@ import com.iasarchi.encurtadorurl.dto.ShortenerRequestDto;
 import com.iasarchi.encurtadorurl.dto.ShortenerResponseDto;
 import com.iasarchi.encurtadorurl.entity.Shortener;
 import com.iasarchi.encurtadorurl.service.ShortenerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/shortener")
 @RestController
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class ShortenerController {
-    @Autowired
-    ShortenerService shortenerService ;
+    private final ShortenerService shortenerService ;
     @PostMapping
     private ResponseEntity<ShortenerResponseDto> save(@RequestBody ShortenerRequestDto shortenerRequestDto) throws Exception{
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         Shortener shortenerReturned = shortenerService.save(shortenerRequestDto.customAlias(), shortenerRequestDto.url());
 
-        ShortenerResponseDto shortenerResponseDto = new ShortenerResponseDto(shortenerReturned);
+        stopWatch.stop();
+
+        ShortenerResponseDto shortenerResponseDto = new ShortenerResponseDto(shortenerReturned, stopWatch.getTotalTimeMillis());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(shortenerResponseDto);
 
--- src/main/java/com/iasarchi/encurtadorurl/dto/ShortenerResponseDto.java
+++ src/main/java/com/iasarchi/encurtadorurl/dto/ShortenerResponseDto.java
@@ -2,8 +2,10 @@ package com.iasarchi.encurtadorurl.dto;
 
 import com.iasarchi.encurtadorurl.entity.Shortener;
 
-public record ShortenerResponseDto(String alias, String url) {
-    public ShortenerResponseDto(Shortener shortener){
-        this(shortener.getId(), shortener.getUrl());
+public record ShortenerResponseDto(String alias, String url, long timeTaken) {
+    public ShortenerResponseDto(Shortener shortener, long timeTaken){
+        this(shortener.getId(), shortener.getUrl(), timeTaken);
     }
+
+
 }
